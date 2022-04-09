e6d3dba3572f323f9c90673796a68f68929e9a4b
KennedyMungai <kennedymungaifmab@gmail.com>
2022-04-09 23:13:46 +0300

    Did some data migrations, created a categorry controller class which in turn lead to making an index.cshtml class

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category>? Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
